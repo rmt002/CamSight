1bf523e35b85593b25c388cd5c673e498dbee52e
rmt002 <roshanmthaliath@gmail.com>
2019-02-12 11:11:51 +0530

    Path added to file indexing

--- VideoRip.py
+++ VideoRip.py
@@ -10,86 +10,89 @@ length = int(input_video.get(cv2.CAP_PROP_FRAME_COUNT))
 fourcc = cv2.VideoWriter_fourcc('M', 'P', 'E', 'G')
 output_video = cv2.VideoWriter('output.avi', fourcc, 25.07, (1280, 720))
 
-current_id=0
-label_ids={}
-person_temp_encoded=[]
+current_id = 0
+label_ids = {}
+person_temp_encoded = []
 
-#Collect and label all the files in the Images folder
-base_dir=os.path.dirname(os.path.abspath(__file__))
-image_dir=os.path.join(base_dir,"Images")
-i=0
-for root,dir,files in os.walk(image_dir):
+# Collect and label all the files in the Images folder
+base_dir = os.path.dirname(os.path.abspath(__file__))
+image_dir = os.path.join(base_dir, "Images")
+i = 0
+for root, dir, files in os.walk(image_dir):
     for file in files:
-        person_temp=face_recognition.load_image_file(file)
-        path=os.path.join(root,file)
+        path = os.path.join(root, file)
         print(path)
-        label=os.path.basename(root).replace(" ","-").lower()
+        print(file)
+        person_temp = face_recognition.load_image_file(path)
+
+        label = os.path.basename(root).replace(" ", "-").lower()
         if not label in label_ids:
             label_ids[label] = current_id
             current_id += 1
         id_ = label_ids[label]
-        person_temp_encoded.append(face_recognition.face_encodings(person_temp)[0])
-        
-#Swap Key and Value in the Dictionary
-labels=dict((v,k) for k,v in label_ids.items())
-print(labels)
+        person_temp_encoded.append(
+            face_recognition.face_encodings(person_temp)[0])
 
-face_locations = []
-face_encodings = []
-face_names = []
-frame_number = 0
+# #Swap Key and Value in the Dictionary
+# labels=dict((v,k) for k,v in label_ids.items())
+# print(labels)
 
-while True:
-    
-    ret, frame = input_video.read()
-    frame_number += 1
+# face_locations = []
+# face_encodings = []
+# face_names = []
+# frame_number = 0
 
-    # If video file is out of frames => quit
-    if not ret:
-        break
+# while True:
 
-    # Convert the image from BGR to RGB 
-    rgb_frame = frame[:, :, ::-1]
+#     ret, frame = input_video.read()
+#     frame_number += 1
 
-    # Find all the faces and face encodings in the current frame of video
-    face_locations = face_recognition.face_locations(rgb_frame)
-    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+#     # If video file is out of frames => quit
+#     if not ret:
+#         break
 
-    face_names = []
+#     # Convert the image from BGR to RGB
+#     rgb_frame = frame[:, :, ::-1]
 
-    for face_encoding in face_encodings:
-        # See if the face is a match for the known face(s)
-        match = face_recognition.compare_faces(
-            person_temp_encoded, face_encoding, tolerance=0.50)
+#     # Find all the faces and face encodings in the current frame of video
+#     face_locations = face_recognition.face_locations(rgb_frame)
+#     face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
 
-       #Search the match array at every frame for a True value and then index it with the labels array for the person's name
-        name = None
-        if match:
-            for index in range(len(match)):
-                if match[i]:
-                    name=labels[i]
+#     face_names = []
 
-        face_names.append(name)
+#     for face_encoding in face_encodings:
+#         # See if the face is a match for the known face(s)
+#         match = face_recognition.compare_faces(
+#             person_temp_encoded, face_encoding, tolerance=0.50)
 
-    # Label the results
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-        if not name:
-            continue
+#        #Search the match array at every frame for a True value and then index it with the labels array for the person's name
+#         name = None
+#         if match:
+#             for index in range(len(match)):
+#                 if match[i]:
+#                     name=labels[i]
 
-        # Drawing the rectangle
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+#         face_names.append(name)
 
-        # Drawing the label
-        cv2.rectangle(frame, (left, bottom - 25),
-                      (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6),
-                    font, 0.5, (255, 255, 255), 1)
+#     # Label the results
+#     for (top, right, bottom, left), name in zip(face_locations, face_names):
+#         if not name:
+#             continue
 
-    # Write to utput video file frame by frame
-    print("Writing frame {} / {}".format(frame_number, length))
-    output_video.write(frame)
+#         # Drawing the rectangle
+#         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-#Release the webcam
-input_video.release()
-cv2.destroyAllWindows()
+#         # Drawing the label
+#         cv2.rectangle(frame, (left, bottom - 25),
+#                       (right, bottom), (0, 0, 255), cv2.FILLED)
+#         font = cv2.FONT_HERSHEY_DUPLEX
+#         cv2.putText(frame, name, (left + 6, bottom - 6),
+#                     font, 0.5, (255, 255, 255), 1)
+
+#     # Write to utput video file frame by frame
+#     print("Writing frame {} / {}".format(frame_number, length))
+#     output_video.write(frame)
+
+# #Release the webcam
+# input_video.release()
+# cv2.destroyAllWindows()
--- VideoStream.py
+++ VideoStream.py
@@ -1,77 +1,83 @@
 import face_recognition
 import cv2
-import os 
+import os
+import PIL
 
-current_id=0
-label_ids={}
-person_temp_encoded=[]
+current_id = 0
+label_ids = {}
+person_temp_encoded = []
 
-#Collect and label all the files in the Images folder
-base_dir=os.path.dirname(os.path.abspath(__file__))
-image_dir=os.path.join(base_dir,"Images")
-i=0
-for root,dir,files in os.walk(image_dir):
+# Collect and label all the files in the Images folder
+base_dir = os.path.dirname(os.path.abspath(__file__))
+print(base_dir)
+image_dir = os.path.join(base_dir, "Images")
+i = 0
+for root, dir, files in os.walk(image_dir):
     for file in files:
-        person_temp=face_recognition.load_image_file(file)
-        path=os.path.join(root,file)
+        path = os.path.join(root, file)
         print(path)
-        label=os.path.basename(root).replace(" ","-").lower()
+        person_temp = face_recognition.load_image_file(path)
+
+        label = os.path.basename(root).replace(" ", "-").lower()
         if not label in label_ids:
             label_ids[label] = current_id
             current_id += 1
         id_ = label_ids[label]
-        person_temp_encoded.append(face_recognition.face_encodings(person_temp)[0])
-        
-#Swap Key and Value in the Dictionary
-labels=dict((v,k) for k,v in label_ids.items())
+        person_temp_encoded.append(
+            face_recognition.face_encodings(person_temp)[0])
+
+# Swap Key and Value in the Dictionary
+labels = dict((v, k) for k, v in label_ids.items())
 print(labels)
 
-face_name=[]
-face_locations=[]
-face_encodings=[]
+face_name = []
+face_locations = []
+face_encodings = []
 
 
-video_capture=cv2.VideoCapture(0)
+video_capture = cv2.VideoCapture(0)
 while True:
-    ret,frame=video_capture.read()
+    ret, frame = video_capture.read()
 
-    #BGR to RGB color arrays
-    color_array= frame[:,:,::-1]
-    
-    face_locations=face_recognition.face_locations(color_array)
-    face_encodings=face_recognition.face_encodings(color_array,face_locations)
-    face_names=[]
+    # BGR to RGB color arrays
+    color_array = frame[:, :, ::-1]
+
+    face_locations = face_recognition.face_locations(color_array)
+    face_encodings = face_recognition.face_encodings(
+        color_array, face_locations)
+    face_names = []
 
     for face_encoding in face_encodings:
-        match=face_recognition.compare_faces(person_temp_encoded,face_encoding,tolerance=0.5)
-        name=None
+        match = face_recognition.compare_faces(
+            person_temp_encoded, face_encoding, tolerance=0.5)
+        name = None
         if match:
+            print(match)
             for index in range(len(match)):
-                if match[i]:
-                    name=labels[i]
-     
+                if match[index]:
+                    name = labels[index]
+
         face_names.append(name)
 
-    #Draw the rectangle t=top r=right b=bottom l=left
-    for (t,r,b,l),name in zip(face_locations,face_names):
+    # Draw the rectangle t=top r=right b=bottom l=left
+    for (t, r, b, l), name in zip(face_locations, face_names):
         if not name:
             continue
-        
-        #(0,0,255) is color code for drawing box around face
-        cv2.rectangle(frame,(l,t),(r,b),(0,0,255),2)
 
-        #Drawing the label
-        cv2.rectangle(frame,(l,b-25),(r,b),(0,0,255),cv2.FILLED)
-        font=cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame,name,(l+6,b-6),font,0.5,(255,255,255),1)
-    
-    #Show the live stream on screen
-    cv2.imshow('Video',frame)
-    
-    #Wait for the keystroke x before quiting the window
-    if cv2.waitKey(10)==ord('x'):
+        # (0,0,255) is color code for drawing box around face
+        cv2.rectangle(frame, (l, t), (r, b), (0, 0, 255), 2)
+
+        # Drawing the label
+        cv2.rectangle(frame, (l, b-25), (r, b), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (l+6, b-6), font, 0.5, (255, 255, 255), 1)
+
+    # Show the live stream on screen
+    cv2.imshow('Video', frame)
+
+    # Wait for the keystroke x before quiting the window
+    if cv2.waitKey(10) == ord('x'):
         break
 
 video_capture.release()
 cv2.destroyAllWindows()
-
